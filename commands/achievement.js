7be4d62592fe55c5b948967812c72e07175a6e2c
CrusherNotDrip <90648119+CrusherNotDrip@users.noreply.github.com>
2022-11-17 23:05:13 +0000

    fixed achievement thingy

--- commands/achievement.js
+++ commands/achievement.js
@@ -7,6 +7,6 @@ module.exports = {
     .addStringOption(option => option.setName('achievement').setDescription('What should the name of the achievement be?').setRequired(true)),
   async execute(interaction) {
     var achievement = interaction.options.getString('achievement');
-    await interaction.reply(`https://minecraftskinstealer.com/achievement/${Math.floor(Math.random() * 40)}/Achievement+Get%21/${achievement}`);
+    await interaction.reply(`https://minecraftskinstealer.com/achievement/${Math.floor(Math.random() * 40)}/Achievement+Get%21/${achievement.replace(" ", "+")}`);
   },
 };
