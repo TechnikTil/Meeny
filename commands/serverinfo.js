76421b8c779c64a9d461b55216e45ec0e2682c46
CrusherNotDrip <90648119+CrusherNotDrip@users.noreply.github.com>
2023-10-28 13:15:28 -0400

    server info (kinda done) + emojis

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -13,10 +13,12 @@ module.exports = {
 				new StringSelectMenuOptionBuilder()
 					.setLabel('Server Info')
 					.setDescription('Information about this server.')
+                    .setEmoji('1167632009452064870')
 					.setValue('serverI'),
 				new StringSelectMenuOptionBuilder()
 					.setLabel('Members Info')
 					.setDescription('Member information about this server.')
+                    .setEmoji('1167627097095807067')
 					.setValue('membersI'),
             );
 
@@ -26,17 +28,44 @@ module.exports = {
         //Embeds
         const infoEmbed = new EmbedBuilder()
             .setTitle(`Server Info`)
+            .setImage(`${interaction.guild.iconURL()}`)
             .setDescription('Select a option!')
             .setFooter({ text: `Requested by: ${interaction.user.username}` })
 
         const serverEmbed = new EmbedBuilder()
-            .setTitle(`Server Info`)
-            .setDescription('WIP!!!!!')
+            .setTitle(`${interaction.guild.name} Info`)
+            .setImage(`${interaction.guild.iconURL()}`)
+            .setDescription(`
+                Name: ${interaction.guild.name} (${interaction.guild.id})
+                Description: ${interaction.guild.description}
+                Created at: ${interaction.guild.createdAt}
+                Region: ${interaction.guild.region}
+                MFA Level: ${interaction.guild.mfaLevel}
+                NSFW Level: ${interaction.guild.nsfwLevel}
+                Rules Channel: ${interaction.guild.rulesChannel} (${interaction.guild.rulesChannelId})
+                Max Bitrate: ${getBitrate()}
+                Verified Server: ${interaction.guild.verified}
+                Partnered Server: ${interaction.guild.partnered}
+                Boost Level: ${interaction.guild.premiumTier}
+                Boost Amount: ${interaction.guild.premiumSubscriptionCount}
+                Vanity URL: ${getVanityURL()}
+            `)
             .setFooter({ text: `Requested by: ${interaction.user.username}` })
 
         const membersEmbed = new EmbedBuilder()
-            .setTitle(`Server Info`)
-            .setDescription('WIP!!!!! 2.0')
+            .setTitle(`${interaction.guild.name} Members Info`)
+            .setImage(`${interaction.guild.iconURL()}`)
+            .setDescription(`
+                Owner: <@${interaction.guild.ownerId}> (${interaction.guild.ownerId})
+                Member Count: ${interaction.guild.memberCount} / ${interaction.guild.maximumMembers}
+                `)
+                /*
+                Members Online:
+                Members Idle:
+                Members DND:
+                Members Offline:
+                Bots: 
+                */
             .setFooter({ text: `Requested by: ${interaction.user.username}` })
 
         const infoReply = await interaction.reply({ embeds: [infoEmbed], components: [row]});
@@ -50,15 +79,28 @@ module.exports = {
                 if (value === "serverI")
                 {
                     await i.update({ embeds: [serverEmbed], components: [row] });
-                    console.log(`${interaction.user.username} selected Server Info! - serverinfo.js`)
+                    console.log(`${interaction.user.username} selected Server Info! for server ${interaction.guild.name} with the ID ${interaction.guild.id} - serverinfo.js`)
                 }
                 if (value === "membersI")
                 {
                     await i.update({ embeds: [membersEmbed], components: [row] });
-                    console.log(`${interaction.user.username} selected Members Info! - serverinfo.js`)
+                    console.log(`${interaction.user.username} selected Members Info! for server ${interaction.guild.name} with the ID ${interaction.guild.id} - serverinfo.js`)
                 }
             }
         });
+
+        function getBitrate()
+        {
+            return `${interaction.guild.maximumBitrate}`.replace('000', 'kbps');
+        }
+
+        function getVanityURL()
+        {
+            if (interaction.guild.vanityURLCode != null)
+                return `${interaction.guild.vanityURLCode} (Used ${interaction.guild.vanityURLUses} times.)`;
+            else
+                return "This server does not have a Vanity URL.";
+        }
 
         console.log(`Command: ${interaction.commandName}, Ran by: ${interaction.user.tag}`);
     },
